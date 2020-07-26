24346c44311b155df07ad2bf2125bd5d32af2e87
KP Shao <kp.shao@outlook.com>
2020-07-26 19:52:24 +0800

    Finish file reader exercise

---
+++ ch10/file_read_exec.py
@@ -0,0 +1,19 @@
+file_name = 'learning_python.txt'
+
+with open(file_name) as file_object:
+    contents = file_object.read()
+    print(contents.rstrip())
+
+with open(file_name) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+lines = [line.replace('Python', 'C') for line in lines]
+for line in lines:
+    print(line.rstrip())
